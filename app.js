0bd3ca38b019d4c45f1f1aff925854f84abefa24
anand <bhardwajanand99@gmail.com>
2021-01-15 14:40:17 +0530

    add file app.js

---
+++ app.js
@@ -0,0 +1,65 @@
+"use strict";
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const path = require('path');
+const expressEjsLayout = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+var cors = require('cors')
+app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist/new-website')));
+app.use(bodyParser.json()).use(morgan());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+require("./mongo");
+require("./models/admin");
+require("./models/student");
+require("./models/teacher");
+require("./models/registration");
+require("./models/bankDetails");
+require("./models/material");
+require("./models/materialsPath");
+require("./models/Category")
+
+app.use("/dashboard", require("./routes/admin"));
+app.use("/dashboard", require("./routes/students"));
+app.use("/dashboard", require("./routes/teachers"));
+app.use("/dashboard", require("./routes/registration"));
+app.use("/dashboard", require("./routes/login"));
+app.use("/dashboard", require("./routes/teachers"));
+app.use("/dashboard", require("./routes/bankDetails"));
+
+app.set("view engine", "ejs");
+app.use(expressEjsLayout);
+app.use((req, res, next) => {
+  req.status = 404;
+  const error = new Error("Routes not found");
+  next(error);
+});
+
+if (app.get("env") === "production") {
+  app.use((error, req, res, next) => {
+    res.status(req.status || 500).send({
+      message: error.message,
+    });
+  });
+}
+
+app.use((error, req, res, next) => {
+  res.status(req.status || 500).send({
+    message: error.message,
+    stack: error.stack,
+  });
+});
+
+const PORT =3300;
+app.listen(PORT, () =>
+  console.log(`..........................server connect on port ${PORT}`)
+);
