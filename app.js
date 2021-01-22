6def78b3c2f0065f95b5a9e3c5eab46a164bfaf1
subodh mishra <subodh@shipgigventures.com>
2021-01-22 15:19:29 +0530

    fggfgfgfg

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var bodyParser = require('body-parser');
 // app.use(bodyParser.urlencoded({limit: '550mb', extended: true}));
 const morgan = require("morgan");
 var cors = require('cors')
-app.use(cors())
+app.use(cors())  
+
+
 // app.use(express.json({limit: '1500mb'}));
 // app.use(express.urlencoded({limit: '150mb'}));
 // app.use(express.urlencoded({ extended: false }));
